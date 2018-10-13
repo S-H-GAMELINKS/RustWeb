76781e5a14c600d2a233d733de0e3bbb275be096
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-13 11:41:39 +0900

    add & using header.vue

--- static/src/index.js
+++ static/src/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue/dist/vue.esm';
+import Header from '../components/layouts/header.vue';
 
 const app = new Vue({
     el: "#app",
+    components: {
+        'nav-bar': Header
+    },
     data() {
         return {
             text: "Awesome Iron!"
