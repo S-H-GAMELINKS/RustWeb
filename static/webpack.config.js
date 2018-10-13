830dd8afad2daa0ea4d8467b8c76f22f5a02c21d
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-13 13:05:35 +0900

    using header.vue components

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,12 +1,23 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
     entry: './src/index.js', 
     output: { 
       filename: 'index.js',     
       path: `${__dirname}/webpack/` 
     },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader'
+        }]
+    },
     resolve: {
         alias: {
           'vue$': 'vue/dist/vue.esm.js'
         }
     },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
   };
